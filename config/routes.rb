69c61f91afc962e36c672f16e85a80b7819249d9
Jean-Philippe Moal <skateinmars@skateinmars.net>
2012-10-20 20:13:51 +0200

    allow dots in repos urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 GithubMates::Application.routes.draw do
   root :to => 'repositories#new'
 
-  match 'repositories/:user/:repo' => 'repositories#show', :as => :repository
+  match 'repositories/:user/:repo' => 'repositories#show', :as => :repository, :constraints => { :repo => /[^\/]*/ }
   resources :repositories, :only => [:create]
 end
