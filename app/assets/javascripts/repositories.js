e2ffdde896b2571484eaa9a524b1b57a0d324d0b
Jean-Philippe Moal <skateinmars@skateinmars.net>
2012-10-22 17:20:25 +0200

    display commiters on a google map

---
+++ app/assets/javascripts/repositories.js
@@ -0,0 +1,71 @@
+function initialize_map(map_element, mapOptions = {}) {
+  var defaultOptions = {
+    panControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+    overviewMapControl: false,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    },
+    zoom: 5,
+    center: new google.maps.LatLng(0, 0),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  window.map = new google.maps.Map(map_element, $.extend(defaultOptions, mapOptions));
+  window.infowindows = [];
+
+  google.maps.event.addListener(map, 'click', close_infowindows);
+}
+
+function close_infowindows() {
+  $.each(infowindows, function(i, infowindow) {
+    infowindow.close();
+  });
+}
+
+function displayUser(dom_element) {
+  var user = {element: $(dom_element), address: $(dom_element).find('.user_location').html()}
+  geocoder.geocode({'address': user.address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var user_position = results[0].geometry.location;
+      var user_login = user.element.find('.user_login').html()
+      var user_gravatar_id = user.element.find('.user_infos img').data('gravatar-id');
+
+      if(((map.center.lat() == 0) && ($('.commiter').has('.user_location').first().find('.user_login').html() == user_login)) || user.element.hasClass('main_commiter')) {
+        map.setCenter(user_position);
+      }
+
+      var infowindow = new google.maps.InfoWindow({
+        content: user.element.html(),
+        maxWidth: 500
+      });
+      infowindows.push(infowindow);
+
+      var marker = new google.maps.Marker({
+        map: map,
+        position: user_position,
+        title: user_login
+        //icon: "http://www.gravatar.com/avatar/"+user_gravatar_id+"?s=30"
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        close_infowindows();
+        infowindow.open(map,marker);
+      });
+
+      user.element.remove();
+    }
+  });
+}
+
+$(document).ready(function(){
+  window.geocoder = new google.maps.Geocoder();
+
+  initialize_map(
+    document.getElementById("commiters_map")
+  );
+
+  $('.commiter').has('.user_location').each(function(i) {
+    displayUser(this);
+  });
+});
