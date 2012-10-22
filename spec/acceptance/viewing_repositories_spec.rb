dfbbc5b732530b565825ae6cdfc3ac1edae06d4b
Jean-Philippe Moal <skateinmars@skateinmars.net>
2012-10-22 19:52:29 +0200

    fix db bugs in the integration test

--- spec/acceptance/viewing_repositories_spec.rb
+++ spec/acceptance/viewing_repositories_spec.rb
@@ -3,22 +3,27 @@ require 'acceptance/acceptance_helper'
 feature 'Viewing repositories', %q{
   In order to see the commiters of a repository
   As a visitor
-  I want to submit a valid repository
-  And see the repository page
+  I want to visit a repository page
+  And see the commiters on a map
 } do
 
-  scenario 'Viewing a repository', :js => true do
+  before do
     VCR.use_cassette('viewing_repositories') do
-      visit '/repositories/rails/acts_as_list'
-
-      page.should have_css('div#commiters_map div.gmnoprint')
-      
-      page.should_not have_content("dhh")
-      page.should_not have_css('#commiters .commiter')
-
-      page.execute_script("google.maps.event.trigger(infowindows[3].marker, 'click');")
-      page.should have_content("David Heinemeier Hansson")
+      Repository.create!(:user => 'rails', :repo => 'acts_as_list')
     end
+  end
+
+  scenario 'Viewing a repository', :js => true do
+    visit '/repositories/rails/acts_as_list'
+    wait_until { page.has_css?("#commiters_map div") }
+
+    page.should have_css('div#commiters_map div.gmnoprint')
+    
+    page.should_not have_content("dhh")
+    page.should_not have_css('#commiters .commiter')
+
+    page.execute_script("google.maps.event.trigger(infowindows[3].marker, 'click');")
+    page.should have_content("David Heinemeier Hansson")
   end
 
 end
